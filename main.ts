056a7258b164eb1f8ebba5abfd54effe33825312
mawako <90530771+mawako@users.noreply.github.com>
2021-12-06 11:56:06 +0000

    Added audio support to game to describe the mood of the game.

--- main.ts
+++ main.ts
@@ -1,13 +1,15 @@
 input.onButtonPressed(Button.A, function () {
     bird.change(LedSpriteProperty.Y, -1)
+    music.playTone(392, music.beat(BeatFraction.Whole))
 })
 input.onButtonPressed(Button.B, function () {
     bird.change(LedSpriteProperty.Y, 1)
+    music.playTone(330, music.beat(BeatFraction.Whole))
 })
 let emptyObstacleY = 0
 let ticks = 0
 let bird: game.LedSprite = null
-let index = 0
+music.playMelody("C D E F G A B C5 ", 303)
 basic.showLeds(`
     . . . . #
     . . . . #
@@ -15,10 +17,11 @@ basic.showLeds(`
     . . . . #
     . . . . #
     `)
+let index = 0
+let obstacles: game.LedSprite[] = []
 bird = game.createSprite(0, 2)
 bird.set(LedSpriteProperty.Blink, 300)
 basic.forever(function () {
-    let obstacles: game.LedSprite[] = []
     while (obstacles.length > 0 && obstacles[0].get(LedSpriteProperty.X) == 0) {
         obstacles.removeAt(0).delete()
     }
@@ -36,6 +39,7 @@ basic.forever(function () {
     for (let obstacle3 of obstacles) {
         if (obstacle3.get(LedSpriteProperty.X) == bird.get(LedSpriteProperty.X) && obstacle3.get(LedSpriteProperty.Y) == bird.get(LedSpriteProperty.Y)) {
             game.gameOver()
+            music.playMelody("C5 A B G A F G E ", 120)
         }
     }
     ticks += 1
